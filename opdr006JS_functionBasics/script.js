63e04cd2406e9fa01fb40f0858a9269fa97a6606
MoniqueDrost <80634091+MoniqueDrost@users.noreply.github.com>
2021-05-06 11:19:02 +0200

    Add files via upload

---
+++ opdr006JS_functionBasics/script.js
@@ -0,0 +1,22 @@
+const makePizza = function () {
+    console.log('Open up the box');
+    console.log('Put the pizza in the oven');
+    console.log('Take the pizza out');
+};
+
+// makePizza();
+
+const makeSushi = function () {
+    console.log('Take seaweed');
+    console.log('Put boiled rice on it');
+    console.log('Put fresh fish on it');
+    console.log('Roll it');
+};
+
+// makeSushi();
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
